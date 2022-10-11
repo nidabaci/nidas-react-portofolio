9dd02166cdb3b863618ae63acfd674820c2c60af
Jonida Baci <jonida.baci@hotmail.com>
2022-10-11 16:23:56 +0200

    feat: imported neccessary libraries

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import react from 'react';
+// Later versons of react dont need this line.
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import './index.css'
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
