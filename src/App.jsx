9c2f09192b9dde3a0450767bb656f1ec3b26dfe4
Jonida Baci <jonida.baci@hotmail.com>
2022-10-11 17:42:53 +0200

    feat: imported all components into app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portofolio from './components/portofolio/Portofolio'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Portofolio />
+    <Services />
+    <Testimonials />
+    <Contact />
+    <Footer />
+    </>
   )
 }
