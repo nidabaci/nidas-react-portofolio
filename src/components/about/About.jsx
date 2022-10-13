759309029fa1da928a0bbc97088e45c035717291
Jonida Baci <jonida.baci@hotmail.com>
2022-10-13 14:08:48 +0200

    fix: fixed div closing tags

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,11 +11,11 @@ const About = () => {
       <h2>About Me</h2>
 
       <div className='container about__container'>
-        <div className='about_me'>
+          <div className='about__me'> 
           <div className="about__me-image">
               <img src={ME} alt="Profile pic" />
           </div>
-
+          </div>
           <div className="about__content">
             <div className="about__cards">
               <article className='about__card'>
@@ -40,9 +40,6 @@ const About = () => {
             <a href="#contact" className='btn btn-primary'>Contact Me</a>
 
           </div>
-
-        </div>
-
       </div>
     </section>
   )
