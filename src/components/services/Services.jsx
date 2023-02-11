4dd231c89bd637af19160b9ef76c1cb089703705
Jonida Baci <jonida.baci@hotmail.com>
2023-02-11 19:34:55 +0100

    feat: Added services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,81 +1,83 @@
-import React from 'react'
-import './services.css'
-import { BiCheck } from 'react-icons/bi'
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <section id='services'>
-      <h5>What I offer</h5>
-      <h2>Services</h2>
+    <section id="services">
+      <h5>More than programming</h5>
+      <h2>My Skills</h2>
       <div className="container services__container">
-        <article className='service'>
-          <div className='service__head'>
-            <h3>UI/UX</h3>
-
+        <article className="service">
+          <div className="service__head">
+            {/* FIRST SERVICE */}
+            <h3>Design</h3>
           </div>
-          <ul className='services__list'>
+          <ul className="services__list">
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <BiCheck className="service__list-icon" />
+              <p>Figma usage for creating mobile/web application design.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <BiCheck className="service__list-icon" />
+              <p>Content creating effectively representing the brand.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <BiCheck className="service__list-icon" />
+              <p>Content and media editing using photoshop.</p>
             </li>
           </ul>
         </article>
-        {/* END OF FIRST SERVICE */}
-        <article className='service'>
-          <div className='service__head'>
-            <h3>Something</h3>
-
+        {/* 2ND SERVICE */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Tools Familiar Using</h3>
           </div>
-          <ul className='services__list'>
+          <ul className="services__list">
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <BiCheck className="service__list-icon" />
+              <p>Effectively using Visual Code and Visual Studio Code</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Comfortable usage of Ionic, Kendo Ui and Prime Faces frameworks
+              </p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Competently using version controls like Git and Azure Devops
+              </p>
             </li>
           </ul>
         </article>
-        {/* END OF SECOND SERVICE */}
-        <article className='service'>
-          <div className='service__head'>
-            <h3>Another Stuff</h3>
-
+        {/* 3RD SERVICE */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Work Demeanor</h3>
           </div>
-          <ul className='services__list'>
+          <ul className="services__list">
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <BiCheck className="service__list-icon" />
+              <p>
+                The ability to work well with others and contribute to a
+                positive team dynamic.
+              </p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <BiCheck className="service__list-icon" />
+              <p>Effective communication</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <BiCheck className="service__list-icon" />
+              <p>The ability to adapt to new technologies and approaches</p>
             </li>
           </ul>
         </article>
-        {/* END OF THIRD SERVICE */}
-
       </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default Services
+export default Services;
