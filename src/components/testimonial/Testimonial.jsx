eb550092f4d1a6c1c874c80524ce47f3225c11db
Jonida Baci <jonida.baci@hotmail.com>
2023-02-11 19:32:35 +0100

    feat: Updated reccommanded section

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './testimonial.css'
-import AVATAR1 from '../../assets/avatar1.jpg'
-import AVATAR2 from '../../assets/avatar2.jpg'
-import AVATAR3 from '../../assets/avatar3.jpg'
+import AVATAR1 from '../../assets/liontara.jpg'
+import AVATAR2 from '../../assets/kevincepani.jpg'
+import AVATAR3 from '../../assets/mbarsilakadiu.jpg'
 
 import { Pagination, Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -13,20 +13,20 @@ import 'swiper/css/pagination';
 const data =[
   {
     avatar: AVATAR1,
-    name: 'Tina Faturina',
-    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias odit expedita rem quod porro corporis eveniet nihil non magnam reiciendis odio voluptates eius esse dignissimos at nulla eligendi perferendis eum obcaecati delectus, distinctio excepturi sequi! Nesciunt quo ipsa expedita eum?' 
+    name: 'Lion Tara - Software Dev',
+    review: 'Only one word that comes to mind when I think of Jonida, and that’s "awesome". We studied at the same university, and I worked with Jonida on many projects, and she was a consummate professional at all times. Jonida‘s work ethic is impeccable, and she is always that one who offers help and support.' 
   },
 
   {
     avatar: AVATAR2,
-    name: 'Lionidha Gjidia',
-    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias odit expedita rem quod porro corporis eveniet nihil non magnam reiciendis odio voluptates eius esse dignissimos at nulla eligendi perferendis eum obcaecati delectus, distinctio excepturi sequi! Nesciunt quo ipsa expedita eum?' 
+    name: 'Kevin Cepani - Wordpress Dev',
+    review: 'I had the privilege of working with Jonida at Intermedia.al where we both were WordPress developers. Jonida excels in WordPress, HTML, CSS and related technologies, and her creativity sets her apart from others. She consistently exceeded project expectations and was a true professional, always going the extra mile for client satisfaction. I highly recommend Jonida.' 
   },
 
   {
     avatar: AVATAR3,
-    name: 'Teo Pateo',
-    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias odit expedita rem quod porro corporis eveniet nihil non magnam reiciendis odio voluptates eius esse dignissimos at nulla eligendi perferendis eum obcaecati delectus, distinctio excepturi sequi! Nesciunt quo ipsa expedita eum?' 
+    name: 'Mbarsila Kadiu - Frontend Dev',
+    review: 'I had the opportunity to mentor and guide Jonida in her professional development while we were involved in a project using React JS and JavaScript. I was truly impressed by her passion for learning and growth. She is an incredibly talented and dedicated individual. Her high spirit, curiosity, and determination to learn made her a valuable asset to our team, and her contributions to the project were invaluable. I highly recommend her to anyone looking for a talented and hardworking individual.' 
   }
 ]
 
@@ -34,8 +34,8 @@ const data =[
 const Testimonial = () => {
   return (
     <section id='testimonial'>
-      <h5>Review From People</h5>
-      <h2>Testimonials</h2>
+      <h5>References from colleagues</h5>
+      <h2>Reccommandations</h2>
 
       <Swiper className="container testimonials__container"
         // install Swiper modules
