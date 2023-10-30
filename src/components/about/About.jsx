cb92bcacf7cb58ffeb92f0a95440c8eae5918e6f
Jonida Baci <jonida.baci@hotmail.com>
2023-10-30 14:52:48 +0100

    small addition to about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,47 +1,54 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/profile-pic.jpg'
-import { BsAward } from 'react-icons/bs'
-import { ImUsers } from 'react-icons/im'
-import { AiOutlineFolderOpen } from 'react-icons/ai'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/profile-pic.jpg";
+import { BsAward } from "react-icons/bs";
+import { ImUsers } from "react-icons/im";
+import { AiOutlineFolderOpen } from "react-icons/ai";
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
-      <div className='container about__container'>
-          <div className='about__me'> 
+      <div className="container about__container">
+        <div className="about__me">
           <div className="about__me-image">
-              <img src={ME} alt="Profile pic" />
+            <img src={ME} alt="Profile pic" />
           </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <ImUsers className="about__icon" />
+              <h5>Languages</h5>
+              <small>5+</small>
+            </article>
+            <article className="about__card">
+              <BsAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>2+ Years</small>
+            </article>
+            <article className="about__card">
+              <AiOutlineFolderOpen className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+</small>
+            </article>
           </div>
-          <div className="about__content">
-            <div className="about__cards">
-              <article className='about__card'>
-                <ImUsers className='about__icon'/>
-                <h5>Languages</h5>
-                <small>5+</small>
-              </article>
-              <article className='about__card'>
-                <BsAward className='about__icon'/>
-                <h5>Experience</h5>
-                <small>2+ Years</small>
-              </article>
-              <article className='about__card'>
-                <AiOutlineFolderOpen className='about__icon'/>
-                <h5>Projects</h5>
-                <small>10+</small>
-              </article>
-            </div>
-            <p>
-            I am a skilled developer with expertise in WordPress, HTML5, CSS3, and making cross-browser compatibile websites. I am proficient in React JS, Ionic, Kendo, and Prime Faces, and have a basic understanding of Next Js, Vue JS, Bootstrap, Python, C#, and Unity. I use Figma to create visually appealing and user-friendly web and mobile application interfaces.            </p>
-            <a href="#contact" className='btn btn-primary'>Contact Me</a>
-
-          </div>
+          <p>
+            I am a skilled developer with expertise in WordPress, HTML5, CSS3,
+            and making cross-browser compatibile websites. I am proficient in
+            React JS, Ionic, Kendo, and Prime Faces, and have a basic
+            understanding of Next Js, Vue JS, Bootstrap, Python, C#, and Unity.
+            I use Figma to create visually appealing and user-friendly web and
+            mobile application interfaces from scratch.{" "}
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Contact Me
+          </a>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
