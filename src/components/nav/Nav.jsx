b2682514729aa768ac07ff51c8b4cf7675051c78
Jonida Baci - Dilaxia Nova <j.baci@dilaxianova.com>
2025-07-04 19:54:36 +0200

    fixed nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,27 +1,84 @@
-import React from 'react'
-import './nav.css'
-import { BiHomeAlt } from 'react-icons/bi'
-import { BiUserPin } from 'react-icons/bi'
-import { AiOutlineBook } from 'react-icons/ai'
-import { MdDesignServices } from 'react-icons/md'
-import { AiFillPhone } from 'react-icons/ai'
-import { BsPaperclip } from 'react-icons/bs'
-import { MdOutlineStars } from 'react-icons/md'
+import React from "react";
+import "./nav.css";
+import { BiHomeAlt } from "react-icons/bi";
+import { BiUserPin } from "react-icons/bi";
+import { AiOutlineBook } from "react-icons/ai";
+import { MdDesignServices } from "react-icons/md";
+import { AiFillPhone } from "react-icons/ai";
+import { BsPaperclip } from "react-icons/bs";
+import { MdOutlineStars } from "react-icons/md";
 
-import { useState} from 'react'
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState("#");
+  const location = useLocation();
+  const isWordpressPage = location.pathname === "/Wordpress-Portofolio";
   return (
     <nav>
-      <a href="/#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt/></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserPin/></a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><AiOutlineBook/></a>
-      <a href="#portofolio" onClick={() => setActiveNav('#portofolio')} className={activeNav === '#portofolio' ? 'active' : ''}><BsPaperclip/></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdDesignServices/></a>
-      <a href="#testimonial" onClick={() => setActiveNav('#testimonial')} className={activeNav === '#testimonial' ? 'active' : ''}><MdOutlineStars/></a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiFillPhone/></a>
-    </nav>
-  )
-}
+      {!isWordpressPage && (
+        <a
+          href="/#"
+          onClick={() => setActiveNav("#")}
+          className={activeNav === "#" ? "active" : ""}
+        >
+          <BiHomeAlt />
+        </a>
+      )}
 
-export default Nav
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <BiUserPin />
+      </a>
+
+      {!isWordpressPage && (
+        <a
+          href="#experience"
+          onClick={() => setActiveNav("#experience")}
+          className={activeNav === "#experience" ? "active" : ""}
+        >
+          <AiOutlineBook />
+        </a>
+      )}
+
+      <a
+        href="#portofolio"
+        onClick={() => setActiveNav("#portofolio")}
+        className={activeNav === "#portofolio" ? "active" : ""}
+      >
+        <BsPaperclip />
+      </a>
+
+      {!isWordpressPage && (
+        <a
+          href="#services"
+          onClick={() => setActiveNav("#services")}
+          className={activeNav === "#services" ? "active" : ""}
+        >
+          <MdDesignServices />
+        </a>
+      )}
+
+      <a
+        href="#testimonial"
+        onClick={() => setActiveNav("#testimonial")}
+        className={activeNav === "#testimonial" ? "active" : ""}
+      >
+        <MdOutlineStars />
+      </a>
+
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiFillPhone />
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
